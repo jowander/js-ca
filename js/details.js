5efd4741a6b9738c01a3997a57e8db3529444807
jowander <74373353+jowander@users.noreply.github.com>
2022-02-19 20:19:18 +0100

    styling and loader

--- js/details.js
+++ js/details.js
@@ -1,6 +1,38 @@
+"use strict"
 const queryString = document.location.search;
-console.log(queryString);
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
-console.log(params);
+console.log(id);
 
+const apiUrl = "https://api.jikan.moe/v3/search/anime?q=naruto" + id;
+
+const errorMessage = document.querySelector(".error");
+const movieDetails = document.querySelector(".movie-details");
+
+async function getAnimeDetails() {
+    try {
+        const response = await fetch(apiUrl);
+        const jsonResults = await response.json();
+
+        // console.log(jsonResults);
+
+        const animeSeriesDetails = jsonResults.results;
+        console.log(animeSeriesDetails);
+
+        movieDetails.innerHTML = "";
+
+        for (let i = 0; i < animeSeriesDetails.length; i++) {
+
+            if (i === 3) {
+                break;
+            }
+
+            movieDetails.innerHTML += `<div><h2 class="movie-name">Movie title: ${animeSeriesDetails[i].title}</h2><img class="movie-img" src="${animeSeriesDetails[i].image_url}"/><h3 class="movie-score">Movie rating: ${animeSeriesDetails[i].score}</h3></div>`;
+        }
+    } catch(e) {
+        errorMessage.innerHTML = `Du har en feil!` + " " + (e);
+    }
+    
+}
+
+getAnimeDetails();
--- js/index.js
+++ js/index.js
@@ -1,32 +1,17 @@
 "use strict"
-const apiUrl = "https://api.jikan.moe/v3/search/anime?q=naruto";
+const selecter = document.querySelector("select");
+const listContainer = document.querySelector(".list");
 
-const newDivs = document.querySelector(".title");
+selecter.addEventListener("change", buildList);
 
-async function getAnimeDetails() {
-    try {
-        const response = await fetch(apiUrl);
-        const jsonResults = await response.json();
+function buildList(event) {
+    const amount = event.target.value;
 
-        // console.log(jsonResults);
+    // console.log(event.target.value);
 
-        const animeSeriesDetails = jsonResults.results;
-        // console.log(animeSeriesDetails);
+    listContainer.innerHTML = "";
 
-        newDivs.innerHTML = "";
-
-        for (let i = 0; i < animeSeriesDetails.length; i++) {
-
-            if (i === 1) {
-                break;
-            }
-
-            newDivs.innerHTML += `<div><h2><a href="details.html/?id=2">${animeSeriesDetails[i].title}</a></h2></div><div><h3><a href="details.html/?id=2"><img src="${animeSeriesDetails[i].image_url}"/></a></h3></div><div><a href="details.html/?id=2">${animeSeriesDetails[i].score}</a></div>`;
-        }
-    } catch(e) {
-        alert(e.message);
+    for (let i = 0; i < amount; i++) {
+        listContainer.innerHTML = `<a class="select-movie" href="details.html?id=${i}">Click for details</a>`
     }
-    
 }
-
-getAnimeDetails();
